7171e473bd02da7bdb7cffcabe286bce9123a94e
duxianghong <2473230617@qq.com>
2021-12-19 00:21:17 +0800

    首次注释,提交文件

--- RunMainWinGridLayout.py
+++ RunMainWinGridLayout.py
@@ -1,11 +1,11 @@
 import sys
-import MainWinVerticalLayout
+import MainWinHVLayout
 from PyQt5.QtWidgets import QApplication,QMainWindow
 
 if __name__=="__main__":
     app=QApplication(sys.argv)
     mainWindow=QMainWindow()
-    ui=MainWinVerticalLayout.Ui_MainWindow()
+    ui=MainWinHVLayout.Ui_MainWindow()
     ui.setupUi(mainWindow)
     mainWindow.show()
     sys.exit(app.exec_())
